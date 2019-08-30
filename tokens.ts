e4f609e04d5cd71541fd89f2c4b45f28d5ccd27e
Chris Bailey <chris@bailey.codes>
2019-08-29 18:15:39 -0700

    fix html attributes

--- tokens.ts
+++ tokens.ts
@@ -8,16 +8,19 @@ if (!input) {
   process.exit;
 }
 
-writeFileSync(input.split('.').slice(0, -1).concat('tokens').join('.'),
-build({ settings: parse(readFileSync(input).toString())
-  .stylesheet.rules.map((rule) => {
-    return {
-      scope: rule.selectors.join(', ').replace(/(^|\s)\./g, '$1'),
-      settings: rule.declarations.reduce((result, declaration) => {
-        return Object.assign(result, {
-          [declaration.property]: declaration.value,
-        });
-      }, {}),
-    };
-  }) }),
+writeFileSync(
+  input.split('.').slice(0, -1).concat('tokens').join('.'),
+  build({
+    settings: parse(readFileSync(input).toString())
+      .stylesheet.rules.map((rule) => {
+        return {
+          scope: rule.selectors.join(', ').replace(/(^|\s)\./g, '$1'),
+          settings: rule.declarations.reduce((result, declaration) => {
+            return Object.assign(result, {
+              [declaration.property]: declaration.value,
+            });
+          }, {}),
+        };
+      })
+  }),
 );
