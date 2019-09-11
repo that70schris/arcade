ebf0bc55327ab2877ccf0d685f7004752e80182a
Chris Bailey <chris@bailey.codes>
2019-09-11 01:18:52 -0700

    sass

--- tokens.ts
+++ tokens.ts
@@ -1,26 +1,22 @@
-import { parse } from 'css';
+import { Declaration, parse, Rule } from 'css';
 import { readFileSync, writeFileSync } from 'fs';
 import { build } from 'plist';
 
-let input = process.argv[2];
-if (!input) {
-  console.log('No Input!');
-  process.exit;
-}
+const input = process.argv[2];
 
 writeFileSync(
   input.split('.').slice(0, -1).concat('tokens').join('.'),
   build({
     settings: parse(readFileSync(input).toString())
-      .stylesheet.rules.map((rule) => {
-        return {
-          scope: rule.selectors.join(', ').replace(/(^|\s)\./g, '$1'),
-          settings: rule.declarations.reduce((result, declaration) => {
-            return Object.assign(result, {
-              [declaration.property]: declaration.value,
-            });
-          }, {}),
-        };
-      })
-  }),
+    .stylesheet.rules.map((rule: Rule) => {
+      return {
+        scope: rule.selectors.join(', ').replace(/(^|\s)\./g, '$1'),
+        settings: rule.declarations.reduce((result, declaration: Declaration) => {
+          return Object.assign(result, {
+            [declaration.property]: declaration.value,
+          });
+        }, {}),
+      };
+    }),
+  } as any),
 );
