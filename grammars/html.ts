ca5827dd8e003155660b462e990942e38d5c8992
Chris Bailey <chris@bailey.codes>
2020-01-13 22:40:52 -0800

    fix variable attributes

--- grammars/html.ts
+++ grammars/html.ts
@@ -1,123 +1,126 @@
 import { writeFileSync } from 'fs';
 import './sugar';
 
-writeFileSync('./dist/html.json', JSON.stringify({
-  injectionSelector: 'L:-comment',
-  scopeName: 'injection.html',
-  repository: {
-    value: {
-      begin: '=',
-      end: /(?<=[^\s=])(?!\s*=)|(?=\/?>)/,
-      beginCaptures: {
-        0: {
-          name: 'punctuation.separator.html',
-        },
-      },
-      patterns: [
-        {
-          begin: /"/,
-          end: /"/,
-          name: 'string',
-          beginCaptures: {
-            0: {
-              name: 'punctuation.definition.string.begin.html',
-            },
-          },
-          endCaptures: {
-            0: {
-              name: 'punctuation.definition.string.end.html',
-            },
+writeFileSync(
+  './dist/html.json',
+  JSON.stringify({
+    injectionSelector: 'L:-comment',
+    scopeName: 'injection.html',
+    repository: {
+      value: {
+        begin: '=',
+        end: /(?<=[^\s=])(?!\s*=)|(?=\/?>)/,
+        beginCaptures: {
+          0: {
+            name: 'punctuation.separator.html',
           },
         },
-      ],
-    },
-  },
-  patterns: [
-    {
-      begin: /{{/,
-      end: /}}/,
-      name: 'meta.embedded.js',
-      beginCaptures: {
-        0: {
-          name: 'punctuation.definition.template-expression.begin.html',
-        },
+        patterns: [
+          {
+            begin: /"/,
+            end: /"/,
+            name: 'string',
+            beginCaptures: {
+              0: {
+                name: 'punctuation.definition.string.begin.html',
+              },
+            },
+            endCaptures: {
+              0: {
+                name: 'punctuation.definition.string.end.html',
+              },
+            },
+          },
+        ],
       },
-      endCaptures: {
-        0: {
-          name: 'punctuation.definition.template-expression.end.html',
-        },
-      },
-      patterns: [
-        {
-          include: 'source.js',
-        },
-      ],
     },
-    {
-      begin: /(\[)([\w\.]+)(\])(?==)/,
-      end: /(?=\s*[^=\s])/,
-      name: 'meta.attribute.input.html',
-      beginCaptures: {
-        1: {
-          name: 'meta.brace.square',
+    patterns: [
+      {
+        begin: /{{/,
+        end: /}}/,
+        name: 'meta.embedded.js',
+        beginCaptures: {
+          0: {
+            name: 'punctuation.definition.template-expression.begin.html',
+          },
         },
-        2: {
-          name: 'entity.other.attribute-name.html',
+        endCaptures: {
+          0: {
+            name: 'punctuation.definition.template-expression.end.html',
+          },
         },
-        3: {
-          name: 'meta.brace.square',
-        },
+        patterns: [
+          {
+            include: 'source.js',
+          },
+        ],
       },
-      patterns: [
-        {
-          include: '#value',
+      {
+        begin: /(\[)([\w\.]+)(\])(?==)/,
+        end: '(?=s*+[^=s])',
+        name: 'meta.attribute.input.html',
+        beginCaptures: {
+          1: {
+            name: 'meta.brace.square',
+          },
+          2: {
+            name: 'entity.other.attribute-name.html',
+          },
+          3: {
+            name: 'meta.brace.square',
+          },
         },
-      ],
-    },
-    {
-      begin: /(\[?)(\()(\w+)(\))(\]?)(?==)/,
-      end: /(?=\s*[^=\s])/,
-      name: 'meta.attribute.output.html',
-      beginCaptures: {
-        1: {
-          name: 'meta.brace.square',
-        },
-        2: {
-          name: 'meta.brace.round',
-        },
-        3: {
-          name: 'entity.other.attribute-name.html',
-        },
-        4: {
-          name: 'meta.brace.round',
-        },
-        5: {
-          name: 'meta.brace.square',
-        },
+        patterns: [
+          {
+            include: '#value',
+          },
+        ],
       },
-      patterns: [
-        {
-          include: '#value',
+      {
+        begin: /(\[?)(\()(\w+)(\))(\]?)(?==)/,
+        end: '(?=s*+[^=s])',
+        name: 'meta.attribute.output.html',
+        beginCaptures: {
+          1: {
+            name: 'meta.brace.square',
+          },
+          2: {
+            name: 'meta.brace.round',
+          },
+          3: {
+            name: 'entity.other.attribute-name.html',
+          },
+          4: {
+            name: 'meta.brace.round',
+          },
+          5: {
+            name: 'meta.brace.square',
+          },
         },
-      ],
-    },
-    {
-      begin: /(\*)(\w+)(?==)/,
-      end: /(?=\s*[^=\s])/,
-      name: 'meta.attribute.control.html',
-      beginCaptures: {
-        1: {
-          name: 'puncuation',
-        },
-        2: {
-          name: 'entity.other.attribute-name.html',
-        },
+        patterns: [
+          {
+            include: '#value',
+          },
+        ],
       },
-      patterns: [
-        {
-          include: '#value',
+      {
+        begin: /(\*)(\w+)/,
+        end: '(?=s*+[^=s])',
+        name: 'meta.attribute.control.html',
+        beginCaptures: {
+          1: {
+            name: 'puncuation',
+          },
+          2: {
+            name: 'entity.other.attribute-name.html',
+          },
         },
-      ],
-    },
-  ],
-}));
+        patterns: [
+          {
+            include: '#value',
+          },
+        ],
+      },
+    ],
+  }),
+);
