9d98510dd5b6380ce7ba501c1bad057171a8a3cd
Chris Bailey <chris@bailey.mx>
2020-11-10 10:52:49 -0700

    add light theme

--- tokens.ts
+++ tokens.ts
@@ -2,21 +2,26 @@ import { Declaration, parse, Rule } from 'css';
 import { readFileSync, writeFileSync } from 'fs';
 import { build } from 'plist';
 
-writeFileSync('./dist/Arcade.tokens', build({
-  settings: parse(readFileSync('./dist/Arcade.css').toString())
-  .stylesheet.rules.map((rule: Rule) => {
-    return {
-      scope: rule.selectors.join(', ').replace(/(^|\s)\./g, '$1').replace(/\\/g, ''),
-      settings: rule.declarations.reduce((result, declaration: Declaration) => {
-        return Object.assign(result, {
-          [(() => {
-            switch (declaration.property) {
-              case 'color': return 'foreground';
-              default: declaration.property;
-            }
-          })()]: declaration.value,
-        });
-      }, {}),
-    };
-  }),
-} as any));
+[
+  'Arcade',
+  'Arcade Light',
+].forEach((name) => {
+  writeFileSync(`./dist/${name}.tokens`, build({
+    settings: parse(readFileSync(`./dist/${name}.css`).toString())
+    .stylesheet.rules.map((rule: Rule) => {
+      return {
+        scope: rule.selectors.join(', ').replace(/(^|\s)\./g, '$1').replace(/\\/g, ''),
+        settings: rule.declarations.reduce((result, declaration: Declaration) => {
+          return Object.assign(result, {
+            [(() => {
+              switch (declaration.property) {
+                case 'color': return 'foreground';
+                default: declaration.property;
+              }
+            })()]: declaration.value,
+          });
+        }, {}),
+      };
+    }),
+  } as any));
+});
